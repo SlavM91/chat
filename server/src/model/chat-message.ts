f618627e96f98965cbe1159b816a2e9f75b70246
vyacheslav_makarov <vyacheslav.makarov@tibbo.com>
2019-12-30 16:50:47 +0300

    Add eslint config file & refactor current code base

--- server/src/model/chat-message.ts
+++ server/src/model/chat-message.ts
@@ -1,8 +1,8 @@
-import {Message} from "./message";
-import {User} from "./user";
+import { Message } from './message';
+import { User } from './user';
 
-export class ChatMessage extends Message{
-    constructor(from: User, content: string) {
-        super(from, content);
-    }
+export class ChatMessage extends Message {
+  constructor(from: User, content: string) {
+    super(from, content);
+  }
 }
